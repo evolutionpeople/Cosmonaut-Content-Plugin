997a4457c5df704df9ea22a8edf70149678ff680
Francesco Colonna <frappacchio@gmail.com>
2017-02-03 17:26:39 +0100

    Modifica dell’autoload SPF

--- cosmonaut.php
+++ cosmonaut.php
@@ -8,78 +8,80 @@ Author: EvolutionPeople
 Author URI: http://www.evolutionpeople.it
 */
 
+
+if (!defined('ABSPATH')) exit;
+
+if (!defined('COSMO_PLUGIN_PATH'))
+    define('COSMO_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
 use Cosmonaut\Content\Creator;
 use Cosmonaut\Content\Rewriter;
 
-if(!defined( 'ABSPATH' ) ) exit;
-
-if(!defined('COSMO_PLUGIN_PATH'))
-    define('COSMO_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
-
-
-
-//require_once COSMO_PLUGIN_PATH.'vendor/autoload.php';
-
-
 
 class Cosmonaut {
+
     function initialize()
     {
-        add_action('admin_init', [$this,'acf_exists']);
+        add_action('admin_init', [$this, 'acf_exists']);
         add_action('init', [$this, 'init'], 5);
     }
+
     function acf_exists()
     {
-        if(!is_plugin_active('advanced-custom-fields-pro/acf.php') && !is_plugin_active('advanced-custom-fields/acf.php'))
+        if (!is_plugin_active('advanced-custom-fields-pro/acf.php') && !is_plugin_active('advanced-custom-fields/acf.php'))
         {
-            add_action( 'admin_notices', [$this,'acf_admin_notice']);
+            add_action('admin_notices', [$this, 'acf_admin_notice']);
         }
     }
+
     function acf_admin_notice()
     {
         $class = 'notice notice-warning';
         $call_to_action_url = 'https://www.advancedcustomfields.com/';
-        $message = __( 'Cosmonaut requires <strong>"Advanced Custom Fields"</strong> to rocks!', 'cosmonaut' );
-        $call_to_action = 'Visit the <a target="_blank" href="'.$call_to_action_url.'">ACF page</a> for more informations.';
-        printf( '<div class="%1$s"><p>%2$s</p><p>%3$s</p></div>', $class, $message,$call_to_action );
+        $message = __('Cosmonaut requires <strong>"Advanced Custom Fields"</strong> to rocks!', 'cosmonaut');
+        $call_to_action = 'Visit the <a target="_blank" href="' . $call_to_action_url . '">ACF page</a> for more informations.';
+        printf('<div class="%1$s"><p>%2$s</p><p>%3$s</p></div>', $class, $message, $call_to_action);
     }
-    function init() {
-        // bail early if a plugin called get_field early
-        if( !did_action('plugins_loaded') ) return;
 
-        require_once COSMO_PLUGIN_PATH.'inc/Creator.class.php';
+    function init()
+    {
+        // bail early if a plugin called get_field early
+        if (!did_action('plugins_loaded')) return;
+
+        //require_once COSMO_PLUGIN_PATH.'inc/Creator.class.php';
         new Creator();
 
-        require_once COSMO_PLUGIN_PATH.'inc/Rewriter.class.php';
+        //require_once COSMO_PLUGIN_PATH.'inc/Rewriter.class.php';
         new Rewriter();
 
-        spl_autoload_register([$this,'autoload']);
+        spl_autoload_register([$this, 'autoload']);
     }
 
-    protected function autoload($classname)
+    public static function autoload($namespace)
     {
-        $params = explode('\\',$classname);
-        if(isset($params[1]))
+        $splitpath = explode('\\', $namespace);
+
+        $filename = implode('/', $splitpath);
+
+        if (file_exists(COSMO_PLUGIN_PATH . $filename . '.php'))
         {
-            switch ($params[1])
-            {
-                case 'Models':
-                    require_once COSMO_PLUGIN_PATH.'models/'.$params[2].'.class.php';
-                    break;
-            }
+            require_once COSMO_PLUGIN_PATH . $filename . '.php';
         }
-
-
     }
 }
 
-function cosmonaut() {
+spl_autoload_register(__NAMESPACE__ . "\\Cosmonaut::autoload");
+
+function cosmonaut()
+{
     global $cosmonaut;
-    if( !isset($cosmonaut) ) {
+    if (!isset($cosmonaut))
+    {
         $cosmonaut = new Cosmonaut();
         $cosmonaut->initialize();
     }
     return $cosmonaut;
 }
+
 cosmonaut();
 
